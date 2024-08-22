39ed5c28c23daccea1946ec9b09424a4f0aaa071
sergiocanar <s.canar@uniandes.edu.co>
2024-08-22 04:19:33 +0000

    add files for dataset

---
+++ BraTs2018_dataset.py
@@ -0,0 +1,50 @@
+import os
+import torch
+import pandas as pd
+from torch.utils.data import Dataset
+
+
+class Brats2018Dataset(Dataset):
+    """
+        Brats2018 task 1 dataset is the segmentation corpus of the data. This dataset class performs dataloading
+        on an already-preprocessed brats2021 data which has been resized, normalized and oriented in (Right, Anterior, Superior) format.
+        The csv file associated with the data has two columns: [data_path, case_name]
+    MRI_TYPE are ["Flair", "T1w", "T1gd", "T2w"] and segmentation label is store separately
+    """
+
+    def __init__(self, root_dir: str, is_train: bool = True, transform=None):
+        """
+        root_dir: path to (BraTS2018_Training_Data) folder
+        is_train: whether or nor it is train or validation
+        transform: composition of the pytorch transforms
+        """
+        super().__init__()
+ 
+        if is_train:
+            csv_fp = os.path.join(root_dir, "train_data.csv")
+        else:
+            csv_fp = os.path.join(root_dir, "val_data.csv")
+
+        self.csv = pd.read_csv(csv_fp)
+        self.transform = transform
+
+    def __len__(self):
+        return self.csv.__len__()
+
+    def __getitem__(self, idx):
+        data_path = self.csv["data_path"][idx]
+        case_name = self.csv["case_name"][idx]
+        # e.g, BRATS_001_modalities.pt
+        # e.g, BRATS_001_label.pt
+                
+        volume_fp = os.path.join(data_path, f"{case_name}_modalities.pt")
+        label_fp = os.path.join(data_path, f"{case_name}_label.pt")
+        # load the preprocessed tensors
+        volume = torch.load(volume_fp)
+        label = torch.load(label_fp)
+        data = {"image": torch.from_numpy(volume).float(), "label": torch.from_numpy(label).float()}
+
+        if self.transform:
+            data = self.transform(data)
+
+        return data
