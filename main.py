1893ff34c4c94d9cb3b9171ca6506b8746726446
DavidTobonIBIO <d.tobonm2@uniandes.edu.co>
2024-08-30 08:39:58 -0500

    trainer for one gpu

--- main.py
+++ main.py
@@ -10,34 +10,36 @@ from build_dataset import build_dataset, build_dataloader
 from model import build_segformer3d_model
 from argument_parser import parser
 from utils.metrics import build_metric_fn
-from trainer_lindo import Brats2018_Trainer
+from trainer_lindo import Brats2018Trainer
 
 args = parser.parse_args()
+
 
 def ddp_setup(rank, world_size, backend="nccl"):
     os.environ["MASTER_ADDR"] = "localhost"
     os.environ["MASTER_PORT"] = "12355"
     init_process_group(backend=backend, rank=rank, world_size=world_size)
-    
+
+
 def main(rank, world_size, epochs, log_interval):
-    
+
     # Initialize the distributed process group
     ddp_setup(rank, world_size, backend="nccl")
-    
+
     # Parse arguments
     args.cuda = not args.no_cuda and torch.cuda.is_available()
-    
+
     # Wandb initialization
-    if args.wandb and rank == 0: # Only the main process initializes wandb
+    if args.wandb and rank == 0:  # Only the main process initializes wandb
         wandb.init(project="3d-segmentation")
         wandb.config.update(args)
-    
+
     # Set random seed
     torch.manual_seed(args.seed)
-    
+
     if args.cuda:
         torch.cuda.manual_seed(args.seed)
-        
+
     # Define data paths
     file_path = os.path.dirname(os.path.abspath(__file__))
     data_path = os.path.join(file_path, "preproc_data")
@@ -47,44 +49,43 @@ def main(rank, world_size, epochs, log_interval):
     # Define path for saving model
     os.makedirs("models", exist_ok=True)
     args.save = os.path.join(file_path, "models", args.save)
-    
+
     # Define dataset and dataloader
     train_dataset = build_dataset(
         dataset_type=args.dataset, root_dir=train_path, is_train=True
     )
-    
+
     valid_dataset = build_dataset(
         dataset_type=args.dataset, root_dir=valid_path, is_train=False
     )
-    
+
     train_loader = build_dataloader(train_dataset)
     valid_loader = build_dataloader(valid_dataset)
-    
+
     # Load the configuration file
     config_path = os.path.join(file_path, "configs", "config.yaml")
     config_dict = yaml.load(open(config_path, "r"), Loader=yaml.FullLoader)
-    
+
     # Define the model
     model = build_segformer3d_model(config=config_dict)
-    
+
     # Model parameters
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
     criterion = build_loss_fn(args.loss)
     training_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1)
-    
+
     # Define metrics
-    
+
     metrics_dict = {
-        'roi': [128, 128, 128],
-        'sw_batch_size': 2,
+        "roi": [128, 128, 128],
+        "sw_batch_size": 2,
     }
-    
+
     metrics_fn = build_metric_fn(
-        metric_type='sliding_window_inference',
-        metric_arg=metrics_dict
+        metric_type="sliding_window_inference", metric_arg=metrics_dict
     )
-    
-    trainer = Brats2018_Trainer(
+
+    trainer = Brats2018Trainer(
         config=config_dict,
         num_epochs=epochs,
         model=model,
@@ -98,14 +99,19 @@ def main(rank, world_size, epochs, log_interval):
         log_interval=log_interval,
         gpu_id=rank,
         save_path=args.save,
-        wandb=args.wandb
+        wandb=args.wandb,
     )
-    
+
     trainer.train()
-    
+
     destroy_process_group()
-    
+
+
 if __name__ == "__main__":
     world_size = torch.cuda.device_count()
-    mp.spawn(main, args=(world_size, args.epochs, args.log_interval), nprocs=world_size, join=True)
-    
+    mp.spawn(
+        main,
+        args=(world_size, args.epochs, args.log_interval),
+        nprocs=world_size,
+        join=True,
+    )
--- trainer_one_gpu.py
+++ trainer_one_gpu.py
@@ -5,8 +5,6 @@ from tqdm import tqdm
 from typing import Dict
 from termcolor import colored
 from torch.utils.data import DataLoader
-from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.distributed import init_process_group, destroy_process_group
 
 from utils.segmentation_metrics import SlidingWindowInference  # TODO asterico
 
@@ -25,132 +23,109 @@ class Brats2018Trainer:
         metrics_fn: SlidingWindowInference,
         training_scheduler: torch.optim.lr_scheduler.StepLR,
         log_interval: int,
-        gpu_id: int,
+        device: torch.device,
         save_path: str,
         wandb: bool,
     ):
-
-        # Config
         self.config = config
 
-        # Data Loaders
         self.train_loader = train_loader
         self.valid_loader = valid_loader
 
-        # Model
-        self.gpu_id = gpu_id
-        self.model = model.to(gpu_id)
-        self.model = DDP(self.model, device_ids=[self.gpu_id])
-
-        # Model parameters
+        self.model = model.to(device)
 
         self.optimizer = optimizer
         self.criterion = criterion
         self.training_scheduler = training_scheduler
         self.num_epochs = num_epochs
 
-        # Losses
         self.best_train_loss = float("inf")
         self.best_val_loss = float("inf")
         self.best_val_dice = 0.0
 
-        # Metrics
         self.compute_metrics = compute_metrics
         self.metrics_fn = metrics_fn
-        # Logging
-        self.print_every = log_interval
+
+        self.log_interval = log_interval
         self.wandb = wandb
 
-        # Path
         self.save_path = save_path
 
     def _train_step(self):
-        torch.cuda.synchronize()
         epoch_avg_loss = 0.0
         self.model.train()
 
         with tqdm(
             total=len(self.train_loader),
-            desc=f"Epoch {self.current_epoch + 1}/{self.num_epochs} Rank:{torch.distributed.get_rank()}",
+            desc=f"Epoch {self.current_epoch + 1}/{self.num_epochs}",
             unit="batch",
         ) as pbar:
             for idx, raw_data in enumerate(self.train_loader):
-                data = raw_data["image"].to(self.gpu_id)
-                labels = raw_data["label"].to(self.gpu_id)
+                data = raw_data["image"].to(self.device)
+                labels = raw_data["label"].to(self.device)
 
                 self.optimizer.zero_grad()
                 preds = self.model(data)
                 loss = self.criterion(preds, labels)
 
-                torch.cuda.synchronize()
-
                 loss.backward()
 
-                # Ensure CUDA operations are synchronized before stepping optimizer
-                torch.cuda.synchronize()
                 self.optimizer.step()
 
                 epoch_avg_loss += loss.item()
-
-                pbar.set_postfix({"train_loss": epoch_avg_loss})
+                pbar.set_postfix({"train_loss": loss.item() / (idx + 1)})
                 pbar.update(1)
 
             epoch_avg_loss /= len(self.train_loader)
-            if epoch_avg_loss < self.best_train_loss:
-                self.best_train_loss = epoch_avg_loss
 
             self.epoch_train_loss = epoch_avg_loss
 
-        torch.cuda.synchronize()
-
-        return epoch_avg_loss
+        # return epoch_avg_loss
 
     def _val_step(self):
-        torch.cuda.synchronize()
         epoch_avg_loss = 0.0
         total_dice = 0.0
 
         self.model.eval()
+
         with torch.no_grad():
             with tqdm(
-                total=len(self.valid_loader), desc=f"Validation", unit="batch"
+                total=len(self.valid_loader), desc="Validation", unit="batch"
             ) as pbar:
                 for idx, raw_data in enumerate(self.valid_loader):
-                    data = raw_data["image"].to(self.gpu_id)
-                    labels = raw_data["label"].to(self.gpu_id)
+                    data = raw_data["image"].to(self.device)
+                    labels = raw_data["label"].to(self.device)
 
                     preds = self.model(data)
                     loss = self.criterion(preds, labels)
+
                     epoch_avg_loss += loss.item()
 
-                    if self.compute_metrics and torch.distributed.get_rank() == 0:
-                        dice = self.metrics_fn(data, labels, model=self.model)
+                    if self.compute_metrics:
+                        dice = self.metrics_fn(preds, labels, model=self.model)
                         total_dice += dice
 
-                    torch.cuda.synchronize()
-
                     pbar.set_postfix(
-                        {"val_loss": epoch_avg_loss, "mean_dice": total_dice}
+                        {
+                            "val_loss": loss.item() / (idx + 1),
+                            "val_dice": total_dice / (idx + 1),
+                        }
                     )
                     pbar.update(1)
 
                 epoch_avg_loss /= len(self.valid_loader)
-                self.epoch_val_loss = epoch_avg_loss
 
                 if self.compute_metrics:
                     total_dice /= len(self.valid_loader)
                     self.epoch_val_dice = total_dice
 
-                if epoch_avg_loss < self.best_val_loss:
-                    self.best_val_loss = epoch_avg_loss
+                self.epoch_val_loss = epoch_avg_loss
 
-        torch.cuda.synchronize()
-
-        return epoch_avg_loss, total_dice
+        # return epoch_avg_loss, total_dice
 
     def _run_train_val(self):
 
-        if self.wandb and torch.distributed.get_rank() == 0:
+        if self.wandb:
             wandb.watch(self.model, criterion=self.criterion, log="all", log_freq=10)
 
         for epoch in range(self.num_epochs):
@@ -158,33 +133,40 @@ class Brats2018Trainer:
             print(colored(f"Epoch {self.current_epoch + 1}/{self.num_epochs}", "green"))
 
             # Training
-            train_loss = self._train_step()
+            self._train_step()
 
             # Validation
-            val_loss, val_dice = self._val_step()
+            self._val_step()
 
             # Update metrics
 
-            self._update_metrics()
+            save_train_checkpoint, save_val_checkpoint = self._update_metrics()
+            if save_train_checkpoint:
+                self._save_checkpoint("train")
+            if save_val_checkpoint:
+                self._save_checkpoint("val")
+
             if self.wandb:
                 self._log_metrics()
 
-            self._save_checkpoint()
-
             self.training_scheduler.step()
 
-        torch.cuda.synchronize()
-
-    def _update_metrics(self) -> None:
+    def _update_metrics(self) -> tuple[bool, bool]:
+        save_train_checkpoint = False
+        save_val_checkpoint = False
         if self.epoch_train_loss <= self.best_train_loss:
             self.best_train_loss = self.epoch_train_loss
+            save_train_checkpoint = True
 
         if self.epoch_val_loss <= self.best_val_loss:
             self.best_val_loss = self.epoch_val_loss
+            save_val_checkpoint = True
 
         if self.compute_metrics:
             if self.epoch_val_dice >= self.best_val_dice:
                 self.best_val_dice = self.epoch_val_dice
+
+        return save_train_checkpoint or save_val_checkpoint
 
     def _log_metrics(self) -> None:
         log_data = {
@@ -196,7 +178,7 @@ class Brats2018Trainer:
         if torch.distributed.get_rank() == 0:
             wandb.log(log_data)
 
-    def _save_checkpoint(self) -> None:
+    def _save_checkpoint(self, split: str) -> None:
 
         torch.save(
             {
@@ -205,9 +187,9 @@ class Brats2018Trainer:
                 "optimizer_state_dict": self.optimizer.state_dict(),
                 "loss": self.epoch_train_loss,
             },
-            self.save_path,
+            os.path.join(self.save_path, f"{split}_checkpoint.pt"),
         )
 
     def train(self):
         self._run_train_val()
-        destroy_process_group()
+        print(colored("Training completed", "green"))
