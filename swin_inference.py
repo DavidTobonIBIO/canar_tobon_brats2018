05fdd3d08ed892bab9058ae00a415036917ce26e
sergiocanar <s.canar@uniandes.edu.co>
2024-10-11 14:27:13 +0000

    hard coded inference

--- swin_inference.py
+++ swin_inference.py
@@ -1,11 +1,17 @@
-import os 
+import os
 import torch
 
-from swin_monai import get_dataloader
+from swin_monai import transforms_swin
 from monai.networks.nets import SwinUNETR
 from functools import partial
 from monai.inferers import sliding_window_inference
-
+from monai.transforms import (
+    LoadImaged, 
+    ConvertToMultiChannelBasedOnBratsClassesd, 
+    NormalizeIntensityd, 
+    ToTensord
+)
+from monai.data import decollate_batch
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,10 +19,15 @@ import matplotlib.pyplot as plt
 # Define paths
 this_file = os.path.abspath(__file__)
 parent_dir = os.path.dirname(this_file)
-data3d_dir = os.path.join(parent_dir, "data3D")
-train_data_dir = os.path.join(data3d_dir, "train_data")
-save_path_monai_data = os.path.join(parent_dir, "swin_monai_data")
-valid_data_dir = os.path.join(save_path_monai_data, "val_data")
+data3d_dir = os.path.join(parent_dir, "data3D", "train_data")
+flair_path = '/home/scanar/BCV_project/canar_tobon_brats2018/data3D/train_data/Brats18_2013_0_1/Brats18_2013_0_1_flair.nii'
+t1_path = '/home/scanar/BCV_project/canar_tobon_brats2018/data3D/train_data/Brats18_2013_0_1/Brats18_2013_0_1_t1.nii'
+t1_ce_path = '/home/scanar/BCV_project/canar_tobon_brats2018/data3D/train_data/Brats18_2013_0_1/Brats18_2013_0_1_t1ce.nii'
+t2_path = '/home/scanar/BCV_project/canar_tobon_brats2018/data3D/train_data/Brats18_2013_0_1/Brats18_2013_0_1_t2.nii'
+seg_path = '/home/scanar/BCV_project/canar_tobon_brats2018/data3D/train_data/Brats18_2013_0_1/Brats18_2013_0_1_seg.nii'
+
+vols_path_lt = [flair_path, t1_path, t1_ce_path, t2_path]
+
 
 # Load model weights
 this_file_dir = os.path.dirname(os.path.abspath(__file__))
@@ -26,8 +37,7 @@ model_weights = torch.load(weight_path)
 # Define device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-
-# Define huyperparameters
+# Define hyperparameters
 roi = (128, 128, 128)
 batch_size = 1
 
@@ -47,8 +57,22 @@ model.load_state_dict(model_weights['model_state_dict'])
 model.to(device)
 model.eval()
 
-# Load data
-valid_dataloader = get_dataloader(valid_data_dir, batch_size, roi, 'valid')
+# Define transforms
+transforms = [
+    LoadImaged(keys=['image', 'label']),
+    ConvertToMultiChannelBasedOnBratsClassesd(keys='label'),
+    NormalizeIntensityd(keys='image', nonzero=True, channel_wise=True),
+    ToTensord(keys=['image', 'label'])
+]
+
+# Load and apply transforms to volume and label
+data_dict = {'image': vols_path_lt, 'label': seg_path}
+for transform in transforms:
+    data_dict = transform(data_dict)
+
+# Extract preprocessed image and label
+image = data_dict['image'].unsqueeze(0).to(device)
+gt = data_dict['label'].unsqueeze(0).to(device)
 
 # Inference
 model_infer = partial(
@@ -60,32 +84,25 @@ model_infer = partial(
 )
 
 with torch.no_grad():
-    for batch_data in valid_dataloader:
-        image = batch_data['image'].to(device)
-        gt = batch_data['label'].to(device)
-        image_np = image.detach().cpu().numpy()
-        gt = gt.squeeze(0)
-        gt_np = gt.detach().cpu().numpy()
-        prob = torch.sigmoid(model_infer(image))
-        seg = prob[0].detach().cpu().numpy()
-        seg = (seg > 0.5).astype(np.uint8)
-        seg_out = np.zeros((seg.shape[1], seg.shape[2], seg.shape[3]))
-        seg_out[seg[1] == 1] = 2
-        seg_out[seg[0] == 1] = 1
-        seg_out[seg[2] == 1] = 4
-        
-        print(seg_out.shape)
-
-        break
+    image_np = image.detach().cpu().numpy()
+    gt = gt.squeeze(0)
+    gt_np = gt.detach().cpu().numpy()
+    prob = torch.sigmoid(model_infer(image))
+    seg = prob[0].detach().cpu().numpy()
+    seg = (seg > 0.5).astype(np.uint8)
+    seg_out = np.zeros((seg.shape[1], seg.shape[2], seg.shape[3]))
+    seg_out[seg[1] == 1] = 2
+    seg_out[seg[0] == 1] = 1
+    seg_out[seg[2] == 1] = 4
+    
+    print(seg_out.shape)
 
 # Save image
 print('Saving image...')
 
-
 first_chanel_vol = image_np[0, 0, :, :]
 first_chanel_vol = (first_chanel_vol - np.min(first_chanel_vol)) / (np.max(first_chanel_vol) - np.min(first_chanel_vol))
 first_chanel_vol = (first_chanel_vol * 255).astype(np.uint8)
-
 
 label_back2og = np.zeros((gt_np.shape[1], gt_np.shape[2], gt_np.shape[3]))
 label_back2og[gt_np[1] == 1] = 2
@@ -97,7 +114,6 @@ assert seg_out.shape == label_back2og.shape == first_chanel_vol.shape
 
 _, _, z = seg_out.shape
 
-
 max_area = 0
 for i in range(z):
     area = np.sum(seg_out[:, :, i] != 0)
@@ -107,7 +123,6 @@ for i in range(z):
 
 print(f'Max area: {max_area} at z = {z_max}')
 
-
 fig, ax = plt.subplots(1, 3, figsize=(15, 5))
 ax[0].imshow(first_chanel_vol[:, :, z_max], cmap='gray')
 ax[0].set_title('Input image')
@@ -116,6 +131,5 @@ ax[1].set_title('Ground truth')
 ax[2].imshow(seg_out[:, :, z_max], cmap='gray')
 ax[2].set_title('Prediction')
 plt.savefig('swin_inference.png')
-
 
 print('Inference done!')
